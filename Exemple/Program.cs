7e2438ba13ddd22b9b6d0d75f812b21a3b751d59
eve <New@domain.com>
2023-03-31 15:57:35 +0500

    Массив изстрок длина которых меньше или равна 3 символам

--- Exemple/Program.cs
+++ Exemple/Program.cs
@@ -20,8 +20,21 @@ void PrintArray(string[] array)
     Console.WriteLine();
 }
 
+void NewArray(string[] array, string[] arr)
+{
+    int row = array.Length;
+    for (int i = 0; i < row; i++)
+    {
+        if (array[i].Length <= 3)
+            arr[i] = array[i];
+    }
+}
+
 
 int row = GetNumber("Введите размер массива  ");
 string[] array = new string[row];
 FillArray(array);
 PrintArray(array);
+string[] arr = new string[row];
+NewArray(array, arr);
+PrintArray(arr);
