2fd85053164f6f5a91f3c9715edc396d2383d629
eve <New@domain.com>
2023-03-31 15:50:28 +0500

    Создание массива

--- Exemple/Program.cs
+++ Exemple/Program.cs
@@ -3,6 +3,18 @@
     Console.Write(message);
     return Convert.ToInt32(Console.ReadLine());
 }
+void FillArray(string[] array)
+{
+    int row = array.Length;
+    for (int i = 0; i < row; i++)
+    {
+        Console.WriteLine($"Введите {i} значение массива ");
+        array[i] = Console.ReadLine();
+    }
+
+}
 
 
 int row = GetNumber("Введите размер массива  ");
+string[] array = new string[row];
+FillArray(array);
